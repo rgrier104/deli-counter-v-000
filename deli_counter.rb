038db56737de0b74a813c1f17afe1a803ecc3215
Rachel Grier <rgrier104@gmail.com>
2018-08-24 14:18:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,14 @@
 # Write your code here.
 def line(array)
+  statement = "The line is currently: "
+  counter = 1
   if array.length == 0
     puts "The line is currently empty."
+  else
+    array.each do |name|
+      statement << "#{counter}. #{name} "
+      counter += 1
+    end
+    puts statement
   end
 end
